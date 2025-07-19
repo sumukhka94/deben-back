faafd746051a35b13a42f6da55936eb7fd4e093f
K A Sumukh <sumukhka94@sumukh.local>
2025-07-19 10:35:38 +0530

    Initial Spring commit

---
+++ src/main/java/com/sumukh/debenback/DebenBackApplication.java
@@ -0,0 +1,13 @@
+package com.sumukh.debenback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebenBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebenBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sumukh/debenback/DebenBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumukh.debenback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebenBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
